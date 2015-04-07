633f1e3a9e0bc0e40417e8f1db06e813e8c03899
lilde90 <leepan1990@gmail.com>
2015-04-07 23:28:58 +0800

    modify atom.c

--- src/atom.c
+++ src/atom.c
@@ -1,8 +1,15 @@
 #include "atom.h"
 #include <limit.h>
+#include <string.h>
+#include "assert.h"
 #define NELEMS(x) ((sizeof (x)) / (sizeof ((x)[0])))
 static struct atom {
     struct atom *link;
     struct len;
     char *str;
 } *buckets[2048];
+
+const char *atom_string(const char *str) {
+    assert(str);
+    return atom_new(str, strlen(str));
+}
